df9893dda29832b4671ede413dea44b24b35f5a0
bytesx <pnada.black.edg@gmail.com>
2021-09-25 02:08:26 -0500

    Funcion de Arreglo , numeros random asignados, obtener numero mayor y menor

---
+++ src/EjercicioArreglos100Num.java
@@ -0,0 +1,38 @@
+import static java.lang.Math.*;
+
+public class EjercicioArreglos100Num {
+
+    public static void main(String[] args) {
+        //Edgar Vazquez - Java - Ejercicio Arreglo con 100 Numeros Enteros
+        // 1.- Crear Arreglo que permita 100 numeros enteros
+        // 2.- Llenar Arreglo con numero aleatorios
+        // int numero = (int)(Math.random() * 100);
+
+        // Declaracion de Arreglo
+        int numEnteros[] = new int[100];
+        int mayor = numEnteros[0];
+        int menor = numEnteros[1];
+        int i, x;
+
+        // Funciona para asignar numeros random al Arreglo
+        for (int numerosIndice = 0; numerosIndice < numEnteros.length; numerosIndice++) {
+            numEnteros[numerosIndice] = (int) (random() * 100);
+            System.out.println(numEnteros[numerosIndice] + " ");
+
+        // Buscar el numero Mayor en el Arreglo
+            for (i = 1; i < numEnteros.length; i++) {
+                if (numEnteros[i] > mayor) {
+                    mayor = numEnteros[i];
+                }
+                for (x = 1; x > numEnteros.length; x++) {
+                    if (numEnteros[x] < menor){
+                        menor = numEnteros[x];
+                    }
+                }
+            }
+        }
+        System.out.println("El numero mayor es " + mayor);
+        System.out.println("El numero menor es " + menor);
+
+    }
+    }
